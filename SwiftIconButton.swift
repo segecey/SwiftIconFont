9a754c3fdc7e8c85012113774ea40bace070f056
Jack Chen <jack@flairtime.com>
2017-07-07 06:45:00 -0400

    SwiftIconButton Icon Title Fix
    Fixed setting UIButton icons directly from IB using SwiftIconButton custom class.

---
+++ SwiftIconButton.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftIconButton.swift
+//  icon
+//
+//  Created by Sedat Gökbek ÇİFTÇİ on 08/07/16.
+//  Copyright © 2016 Sedat Gökbek ÇİFTÇİ. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SwiftIconButton: UIButton {    
+    @IBInspectable var Icon: String = "" {
+        didSet {
+            self.parseIcon()
+            self.setTitle(Icon, for: .normal)
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.parseIcon()
+    }
+
+}
+
+
